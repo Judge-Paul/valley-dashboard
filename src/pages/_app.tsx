81b7d456713c7ecfd66c49cb5fe60b2dce47c095
Judge-Paul <judgepaulogebe3@gmail.com>
2024-03-03 08:09:18 +0100

    chore: add title and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "@/components/ui/toaster";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/icon.png" />
+        <title>Valley Dashboard</title>
+      </Head>
       <Toaster />
       <Component {...pageProps} />
     </>
